b63fd9d37653df1d83dbe37633aac641f56d7b4b
Cheng Huang <huangchengadam@gmail.com>
2021-11-17 22:16:13 +0100

    add data from remote api

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,36 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import PubSub from 'pubsub-js'
 import './Header.css'
 
 export default class Header extends Component {
+
+    getData = ()=>{
+        const {inputButton:{value:keyword}} = this
+        axios({
+                method:'GET',
+                url:'https://api.github.com/search/users',
+                params:{
+                    q:keyword
+                }
+            }).then(
+                response=>{
+                    console.log(response.data.items)
+                    PubSub.publish('userdata', {users:response.data.items})
+                },
+                error=>{
+                }
+        )
+    }
+
     render() {
+        console.log("??")
         return (
             <div className='search-container clearfix'>
                 <h2 className='title'>Search github users</h2>
                 <div className='search-area'>
-                    <input type="text" placeholder='Please enter the username'/>
-                    <button>Search</button>
+                    <input ref = {(inputButton)=>{this.inputButton = inputButton}}  type="text" placeholder='Please enter the username'/>
+                    <button onClick={this.getData}>Search</button>
                 </div>
             </div>
         )
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react'
+import PubSub from 'pubsub-js'
 import './List.css'
 export default class List extends Component {
+
+    state = {
+        users:[]
+    }
+    componentDidMount(){
+        PubSub.subscribe('userdata',(msg, data)=>{
+            console.log("data = ", data)
+            this.setState({...data})
+        })
+        console.log(this.state.users)
+    }
+
     render() {
+        console.log("??")
+        const {users} = this.state
+        console.log("users = ", users)
         return (
             <div className='list-wrapper clearfix'>
-                <div className='item'>
-                    <a href="">
-                        <img src="" alt="" />
-                    </a>
-                    <span>koutei</span>
-                </div>
+            {
+                users.map((userObj)=>{
+                    return (
+                        <div key={userObj.id} className='item'>
+                            <a href={userObj.html_url} target='_blank' rel='noreferrer'>
+                                <img  src={userObj.avatar_url} alt='profile' />
+                            </a>
+                            <span>{userObj.login}</span>
+                        </div>
+                    )
+                })
+            }
             </div>
         )
     }
